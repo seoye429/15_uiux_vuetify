726de73fd78cbfd07e6565a9362be5c6fa174cde
seoye429 <seoyei77@gmail.com>
2025-05-30 17:02:50 +0900

    Add files via upload

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+  <v-app>
+    <v-btn>
+      Button
+    </v-btn>
+  </v-app>
+  </NuxtLayout>
+</template>
+
+<!--
+nuxt누스트:파일 기반 라이팅 SSR 컴포넌트 등 풀스택 vue 앱개발을 하기 위한
+    오픈소스 프레임워크 임,
+-->
+
+
